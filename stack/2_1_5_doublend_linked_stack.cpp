e67b03fdc27879ecd608ce0c93f0d26cf92eb04b
TMAstrider <t.m.a.strider@gmail.com>
2023-09-11 13:44:36 +0800

    implemented the stack section

--- stack/2_1_5_doublend_linked_stack.cpp
+++ stack/2_1_5_doublend_linked_stack.cpp
@@ -8,9 +8,10 @@ typedef struct DbSNode{			    //定义双链表结点类型
     int data;				        //每个节点存放一个数据元素
     struct DbSNode *last,*next;	    //指向前后两个结点
 }DbSNode;
-
-typedef struct DbLiStack{	        //双链表实现的栈（栈顶在链尾）
-    struct DbSNode *head, *rear;    //两个指针，分别指向链头、链尾
+// 双链表实现的栈（栈顶在链尾）
+// 两个指针，分别指向链头 *head、链尾*rear
+typedef struct DbLiStack{	        
+    struct DbSNode *head, *rear;    
 }DbLiStack, *DbStack;
 
 
@@ -20,13 +21,13 @@ typedef struct DbLiStack{	        //双链表实现的栈（栈顶在链尾）
 // bool Pop(DbSNode &S, int &x);
 // bool isStackEmpty(DbSNode S);
 
-bool InitStack(DbSNode &S);
+bool InitStack(DbStack &S);
 
-bool Push(DbSNode &S, int x);
+bool Push(DbStack &S, int x);
 
-bool Pop(DbSNode &S, int &x);
+bool Pop(DbStack &S, int &x);
 
-bool isStackEmpty(DbSNode S);
+bool isStackEmpty(DbStack S);
 
 int main() {
 
@@ -34,8 +35,58 @@ int main() {
     return 0;
 }
 
-bool InitStack(DbSNode &S) {
-    DbSNode *S = (DbSNode *)malloc(sizeof(DbSNode));
+bool InitStack(DbStack &S) {
+    S = (DbLiStack *)malloc(sizeof(DbLiStack));
+    // 分配一个链栈（双链表实现，栈顶在链尾）
+    DbSNode *p = (DbSNode *)malloc(sizeof(DbSNode));
+    // 新建一个头节点
+    p->next = NULL;
+    p->last = NULL;
 
+    S->head = p;
+    S->rear = p;
+
+    return true;
+}
+
+bool Push(DbStack &S, int x) {
+    DbSNode *p = (DbSNode *) malloc(sizeof(DbSNode));
+    p->data = x;
+    p->next = NULL;
+    p->last = S->rear;
+    S->rear->next = p;
+    S->rear = p;
+    return true;
+}
+
+bool Pop(DbStack &S, int &x) {
+    if(isStackEmpty(S))
+        return false;
+    DbSNode *p = S->rear;
+    S->rear = p->last;
+    S->rear->next = NULL;
+    free(p);
+    
+
+    return true;
+}
+
+
+bool Pop(DbStack &S, int &x) {
+    if(isStackEmpty(S))
+        return false;
+    DbSNode *p = S->rear;
+    
+    S->rear = S->rear->last;
+    free(S->rear->next);
+    S->rear->next =NULL;
+
+    return true;
+}
+
+
+bool isStackEmpty(DbStack S) {
+    if(S->head == S->rear)
+        return true;
     return false;
 }
